5c55e9c6f8ab23b70df13cfa13b412eb2ecc9e19
Griffin Friedman <grif.friedman@gmail.com>
2020-03-31 11:21:31 -0400

    Make timer animate

--- hourglass.js
+++ hourglass.js
@@ -5,7 +5,7 @@ export class Hourglass {
         this.ctx = this.canvas.getContext('2d');
         this.width = this.ctx.canvas.width;
         this.height = this.ctx.canvas.height;
-
+        this.currentTime = 0; 
         this.resetCanvas();
     }
 
@@ -13,19 +13,49 @@ export class Hourglass {
         this.ctx.clearRect(0,0,this.width,this.height);
         this.ctx.fillStyle = '#444';
         this.ctx.fillRect(0, 0, this.width, this.height);
+        this.drawDays();
+    }
+
+    drawDays() {
+        const secondsPerDay = 24 * 60 * 60; 
+        const rowsPerDay = secondsPerDay / this.width;
+        
+        // Calculate how many days tall the canvas is
+        const days = this.height / rowsPerDay;
+
+        // Iterate through the days 
+        for (let day = 1; day <= days; day++) {
+
+            // Draw the day line
+            this.ctx.fillStyle = '#888';
+            this.ctx.fillRect(0, day * rowsPerDay, this.width, 1)
+        }
+
+        // console.log(rowsPer/Day);
     }
 
     updateTime(time) {
-        this.resetCanvas();
+        // this.resetCanvas();
 
         // Draw the new pixels
         const rows = Math.floor(time / this.width);
         const remainder = time%this.width;
 
         // Fill in the pixels
-        this.ctx.fillStyle = '#888';
+        this.ctx.fillStyle = 'white';
         this.ctx.fillRect(0,0, this.width, rows);
         this.ctx.fillRect(0,rows, remainder, 1)
+    }
+
+    startAnimating(startTime) {
+        this.currentTime = startTime;
+        this.updateTime(this.currentTime);
+
+        window.setInterval(() => {
+            this.currentTime++;
+            this.updateTime(this.currentTime);
+        }, 1000)
+
     }
 
 
--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import {Hourglass} from './hourglass.js';
 const canvas = document.getElementById('hourglass');
 const hourglass = new Hourglass(canvas);
 
-hourglass.updateTime(28471);
+hourglass.startAnimating(142010);
 
 // console.log(hourglass)
